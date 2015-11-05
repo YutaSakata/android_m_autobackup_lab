6eeeabc222f0747b20c1de7f4fb8c09300f9fdc4
Yuta Sakata <yusaka39@gmail.com>
2015-11-05 22:37:59 +0900

    Opt imports

--- app/src/main/java/com/example/yusaka/autobackuptest/MainActivity.java
+++ app/src/main/java/com/example/yusaka/autobackuptest/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.yusaka.autobackuptest;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
